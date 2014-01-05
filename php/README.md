c0ced13fb2e58990943db86b7d4481df90f235f3
NeoAtlantis <rijndael@yeah.net>
2014-01-06 05:11:19 +0800

    save

---
+++ php/README.md
@@ -0,0 +1,12 @@
+PHP Implementation of NPBS
+==========================
+
+Dependencies
+------------
+
+1. `captcha` library requires a font to display Chinese characters, when one
+   want to send a message by web interface. Choose your font and put it to
+   `./securimage/` with name `font.ttf`.
+
+2. `php-gd` library for producing images. Or you may ignore this item and the
+   previous and comment out captcha.
--- php/captcha.php
+++ php/captcha.php
@@ -9,9 +9,11 @@ $wortschatz = '
 想作种开美总从无情己面最女但现前些所同日手又行意动
 ';
 
+$fontname = dirname(__FILE__) . '/securimage/font.ttf';
+
 $image = new Securimage();
 
-$image->ttf_file        = './securimage/3.ttf';
+$image->ttf_file        = $fontname;
 if(rand(0,100) < 10)    // great possibility is a good reason for refreshing
     $image->captcha_type = Securimage::SI_CAPTCHA_MATHEMATIC;
 
@@ -20,7 +22,6 @@ $image->image_height    = 70;                                // width in pixels 
 $image->image_width     = $image->image_height * M_E;          // a good formula for image size
 $image->perturbation    = 0.8;                               // 1.0 = high distortion, higher numbers = more distortion
 $image->image_bg_color  = new Securimage_Color("#CCCCCC");   // image background color
-#$image->background_directory = "./securimage/backgrounds";
 $image->text_color      = new Securimage_Color("#000000");   // captcha text color
 $image->num_lines       = 8;                                 // how many lines to draw over the image
 $image->line_color      = new Securimage_Color("#000000");   // color of lines over the image
@@ -33,7 +34,7 @@ $image->charset = str_split(
 $image->code_length	= 2;
 #$image->image_type      = SI_IMAGE_JPEG;                     // render as a jpeg image
 $image->image_signature = 'NPBS Entrance';
-$image->signature_font = './securimage/3.ttf';
+$image->signature_font = $fontname;
 $image->signature_color = new Securimage_Color(rand(0, 64),
                                              rand(64, 128),
                                              rand(128, 255));  // random signature color
